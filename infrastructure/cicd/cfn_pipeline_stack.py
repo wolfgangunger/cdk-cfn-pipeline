f2c881108f039d282129a9076fb857279e696354
Wolfgang Unger <wolfgang.unger@gmx.de>
2023-08-13 18:06:10 -0300

    iam

--- infrastructure/cicd/cfn_pipeline_stack.py
+++ infrastructure/cicd/cfn_pipeline_stack.py
@@ -89,7 +89,7 @@ class CfnPipelineStack(Stack):
                                 "cfn_001_to_be_replaced/vars_dev.json"
                             ),
                             run_order=1,
-                            role=self.cfn_deploy_role
+                            #role=self.cfn_deploy_role
                             # cfn_capabilities=["CAPABILITY_IAM","CAPABILITY_NAMED_IAM"]
                             # cfn_capabilities=[CfnCapabilities.NAMED_IAM]
                             # cfn_capabilities=cfn_capabilities
