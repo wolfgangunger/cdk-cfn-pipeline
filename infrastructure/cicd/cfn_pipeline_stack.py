7f7bb649172efe51071870357156b29266e79d1e
Wolfgang Unger <wolfgang.unger@gmx.de>
2023-08-13 17:19:29 -0300

    iam update

--- infrastructure/cicd/cfn_pipeline_stack.py
+++ infrastructure/cicd/cfn_pipeline_stack.py
@@ -86,7 +86,7 @@ class CfnPipelineStack(Stack):
                     template_path=source_output.at_path("cfn_001_to_be_replaced/template.yaml"),
                     template_configuration=source_output.at_path("cfn_001_to_be_replaced/vars_dev.json"),
                     run_order=1,
-                    #role=self.cfn_deploy_role 
+                    role=self.cfn_deploy_role 
                     #cfn_capabilities=["CAPABILITY_IAM","CAPABILITY_NAMED_IAM"]
                     #cfn_capabilities=[CfnCapabilities.NAMED_IAM]
                     #cfn_capabilities=cfn_capabilities     
